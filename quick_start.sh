d8b577f5705c213c931b35c1192395de5aa21001
Bruce Jin <brucejin@lilith.com>
2024-04-24 12:08:46 +0000

    fix bug

--- quick_start.sh
+++ quick_start.sh
@@ -11,20 +11,20 @@ python src/train/train_sovits.py \
  -c src/configs/sovits.json \
  -n varus \
  -t sovits \
- -e 50 \
+ -e 25 \
  -lr 0.4 \
  -bs 16 \
  -nw 0 \
- --save_every_epoch 10 \
+ --save_every_epoch 5 \
  --keep_ckpts 5 \
 
 # train gpt mpdel
 python src/train/train_gpt.py \
  -c src/configs/s1longer.yaml \
  -n varus \
- -e 50 \
+ -e 25 \
  -bs 16 \
  -nw 0 \
- --save_every_epoch 10
+ --save_every_epoch 5
 
 
