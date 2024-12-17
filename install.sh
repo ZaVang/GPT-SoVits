5ba2514ee9f6ee0f85defec288bb06f926f8d3ba
Bruce Jin <brucejin@lilith.com>
2024-12-17 07:50:04 +0000

    downgrade torch

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 conda install -c conda-forge gcc
 conda install -c conda-forge gxx
 conda install ffmpeg cmake
-conda install pytorch==2.5.0 torchvision==0.20.0 torchaudio==2.5.0 pytorch-cuda=12.1 -c pytorch -c nvidia
+conda install pytorch==2.1.0 torchvision==0.20.0 torchaudio==2.1.0 pytorch-cuda=12.1 -c pytorch -c nvidia
 pip install -r requirements.txt
